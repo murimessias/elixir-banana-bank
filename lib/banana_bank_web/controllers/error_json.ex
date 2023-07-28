7a4af698347b8431c7b0928f47c2b2a7b39d3479
Murilo Messias de Camargo <murilo.messias@gmail.com>
2023-07-28 17:03:24 -0300

    feat: add show user

--- lib/banana_bank_web/controllers/error_json.ex
+++ lib/banana_bank_web/controllers/error_json.ex
@@ -13,6 +13,14 @@ defmodule BananaBankWeb.ErrorJSON do
     %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
   end
 
+def error(%{status: :not_found}) do
+    %{
+      status: :not_found,
+      message: "User not found"
+    }
+  end
+
+
   def error(%{changeset: changeset}) do
     %{
       errors:
